049208c7185bef984e29481260c5b1c8a972e6da
Tozarin <89902951+Tozarin@users.noreply.github.com>
2022-03-15 01:46:22 +0300

    Added another backups

--- Program.cs
+++ Program.cs
@@ -1,24 +1,72 @@
 ﻿using System;
 using static System.Console;
+using System.Linq;
+using System.Collections.Generic;
 
-namespace TaskTests
+namespace Tests
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Deck deck = new Deck();
-            Shoe shoe = new Shoe(3);
-            Card card1 = new Card("9", "Q");
-            Card card2 = new Card("A", "B");
-            Hand hand = new Hand(card1, card2);
-            WriteLine(hand.GetScore());
+            string messenge = "abcbbbbbacabbacddacdbbaccbbadadaddd abcccccbacabbacbbaddbdaccbbddadadcc bcabbcdabacbbacbbddcbbaccbbdbdadaac";
 
-            hand.AddCard(new Card("A", "D"));
-            WriteLine(hand.GetScore());
+            double[] p = { 0.23, 0.31, 0.23, 0.19, 0.02, 0.02 };
 
-            WriteLine("Ended");
-            ReadKey();
+            List<Letters> letters = new List<Letters>();
+
+            for(int i = 0; i < 6; i++)
+            {
+                letters.Add(new Letters(p[i], i));
+            }
+
+            for(int _ = 0; _ < 5; _++)
+            {
+                double mn = 1;
+                int index = 0;
+                for(int i = 0; i < 6; i++)
+                {
+                    if (letters[i].P <= mn)
+                    {
+                        index = i;
+                        mn = letters[i].P;
+                    }
+                }
+
+                double mn2 = 1;
+                int index2 = 0;
+                for(int i = 0; i < 6; i++)
+                {
+                    if(letters[i].P <= mn2 && !(letters[index].OtherElements.Contains(i)))
+                    {
+                        index2 = i;
+                        mn2 = letters[i].P;
+                    }
+                }
+
+                foreach(int i in letters[index].OtherElements)
+                {
+                    letters[i].P = mn + mn2;
+                    letters[i].Code += "1";
+                }
+
+                foreach (int i in letters[index2].OtherElements)
+                {
+                    letters[i].P = mn + mn2;
+                    letters[i].Code += "0";
+                }
+
+                letters[index].OtherElements = letters[index].OtherElements.Concat(letters[index2].OtherElements).ToList();
+                letters[index2].OtherElements = letters[index].OtherElements;
+            }
+
+            foreach (Letters l in letters)
+            {
+                string str = new string(l.Code.Reverse().ToArray());
+                WriteLine(str);
+            }
+
         }
+
     }
 }
