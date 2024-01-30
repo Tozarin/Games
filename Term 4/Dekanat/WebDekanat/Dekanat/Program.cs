4ea85d63515c2eee20d813aeb51d032396391496
Startcev <tozarin@mail.ru>
2024-01-30 18:59:24 +0300

    adds 4th term hws
    Signed-off-by: Startcev <tozarin@mail.ru>

---
+++ Term 4/Dekanat/WebDekanat/Dekanat/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using Dekanat.DekanatLib;
+using Dekanat.DekanatLib.PhasedCuckooHashSet;
+using Dekanat.DekanatLib.StripedHashSet;
+
+namespace Dekanat
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
